6a64c6392eb29e259a797fcd053c06e8efb00a86
Simon Tucker <brotherlogic@gmail.com>
2023-01-10 20:35:16 +0000

    Add in install roon core

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-sudo apt install emacs
+sudo apt update
+sudo apt install -y emacs zsh golang
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "path+=('/home/simon/go/bin/')" >> ~/.zshrc
+
+
+/home/simon/config/refresh.sh
+
+wget https://download.roonlabs.net/builds/roonserver-installer-linuxx64.sh
+./roonserver-installer-linuxx64.sh
