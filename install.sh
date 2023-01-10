00db269fc64f86ac94f798965ce7924f10174bd4
Simon Tucker <brotherlogic@gmail.com>
2023-01-10 20:37:42 +0000

    Updates to support roon

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install -y emacs zsh golang
+sudo apt install -y emacs zsh golang ffmpeg cifs-utils
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "path+=('/home/simon/go/bin/')" >> ~/.zshrc
 
@@ -7,4 +7,6 @@ echo "path+=('/home/simon/go/bin/')" >> ~/.zshrc
 /home/simon/config/refresh.sh
 
 wget https://download.roonlabs.net/builds/roonserver-installer-linuxx64.sh
-./roonserver-installer-linuxx64.sh
+
+chmod u+x roonserver-installer-linuxx64.sh
+sudo ./roonserver-installer-linuxx64.sh
