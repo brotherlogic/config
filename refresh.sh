d07ab5cf3d851e929f3c7ba6152f16ea6b8b5adb
Simon Tucker <brotherlogic@gmail.com>
2023-01-12 14:51:23 -0800

    Adds budget

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordadder
+for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordadder recordbudget
 do
     go install "github.com/brotherlogic/$job/${job}_cli@latest"
 done
