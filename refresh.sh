56bdf9532194785559bd9e058147b6eebc551c0e
Simon Tucker <brotherlogic@gmail.com>
2023-01-15 09:41:23 -0800

    Add bcs

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget
+for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget bandcampserver
 do
     go install "github.com/brotherlogic/$job/${job}_cli@latest"
 done
