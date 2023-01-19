c644b70e744260f9eb173e6db7235dc7271b69ba
Simon Tucker <brotherlogic@gmail.com>
2023-01-19 08:32:20 -0800

    Sets bcl to be private repo

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ sudo timedatectl set-timezone America/Los_Angeles
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+go env -w GOPRIVATE=github.com/brotherlogic/bandcamplib
