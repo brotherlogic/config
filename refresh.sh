95960b6b5218627ddb403683f4515f7a7a9acabd
Simon Tucker <brotherlogic@gmail.com>
2023-01-31 17:10:36 -0800

    Adds validator

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget bandcampserver cdprocessor adventserver githubcard tasklist queue discovery recordsorganiser recordsales
+for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget bandcampserver cdprocessor adventserver githubcard tasklist queue discovery recordsorganiser recordsales recordvalidator
 do
     go install "github.com/brotherlogic/$job/${job}_cli@latest"
 done
