99340b620543258f1389026dee5b8112464092bf
Simon Tucker <brotherlogic@gmail.com>
2023-02-17 11:44:18 -0800

    Add wants

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget bandcampserver cdprocessor adventserver githubcard tasklist queue discovery recordsorganiser recordsales recordvalidator wantslist
+for job in recordcollection recordgetter logging gobuildslave buildserver recordcleaner recordfanout focus recordadder recordbudget bandcampserver cdprocessor adventserver githubcard tasklist queue discovery recordsorganiser recordsales recordvalidator wantslist recordwants
 
 do
     go install "github.com/brotherlogic/$job/${job}_cli@latest"
